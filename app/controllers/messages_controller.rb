cfd80e41fd2f97de8ac6ae9f133ac0641a336631
TakumiOtsuki-417 <heitaku443@gmail.com>
2021-05-13 18:25:40 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,22 @@
 class MessagesController < ApplicationController
   def index
+    @room = Room.find(params[:room_id])
+    @message = Message.new
   end
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(form_message_content)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def form_message_content
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+  end
+
 end
